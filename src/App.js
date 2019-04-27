3ce9f153ef8a3a4239031218014d02e098e47786
Thomas <thomas@craftacademy.se>
2019-04-27 12:39:20 +0200

    just some fun with hello world...

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    counter: 0
+  }
+
+
+  increment() {
+    this.setState({ counter: this.state.counter += 1 })
+  }
+
+
+  render() {
+    return (
+      <>
+        <h1>Hello {this.props.name}</h1>
+        {this.state.counter}
+        <button onClick={this.increment.bind(this)}>Click me</button>
+      </>
+    );
+  }
+
 }
 
 export default App;
